c04b85f54f64c69ca64f48ba84f8e4be4afc566d
Brocolio de la chunsa <lpradop@unsa.edu.pe>
2020-10-11 23:20:24 -0500

    const correctness

--- square_matrix.hpp
+++ square_matrix.hpp
@@ -1,14 +1,14 @@
 #pragma once
 #include <initializer_list>
 #include <iostream>
-//prototipo de clase, quiza presente bugs, lo ire mejorando poco a poco.
-//TODO implementar destructor
-template <class DataType, int size> class SquareMatrix {
+// prototipo de clase, quiza presente bugs, lo ire mejorando poco a poco.
+// TODO implementar destructor
+template <class DataType, std::size_t size> class SquareMatrix {
 private:
   DataType **matrix_;
   static DataType ProductElement(const SquareMatrix<DataType, size> &a,
-                                 const SquareMatrix<DataType, size> &b, int i,
-                                 int j);
+                                 const SquareMatrix<DataType, size> &b,
+                                 const int i, const int j);
 
 public:
   SquareMatrix(std::initializer_list<std::initializer_list<DataType>> matrix);
@@ -16,7 +16,8 @@ public:
   SquareMatrix() = delete;
   ~SquareMatrix();
 
-  void Print();
+  void Print() const;
+
   SquareMatrix<DataType, size> &
   operator*=(const SquareMatrix<DataType, size> &rhs) {
     auto a = *this;
@@ -36,24 +37,22 @@ public:
     return lhs;
   }
 
-  DataType *operator[](int i) const {
+  DataType *operator[](const int i) const {
     if (i > size) {
       std::cout << "Index out of bounds" << std::endl;
       return matrix_[i];
     }
-
     return matrix_[i];
   }
 };
 
-template <class DataType, int size>
+template <class DataType, std::size_t size>
 SquareMatrix<DataType, size>::SquareMatrix(
     std::initializer_list<std::initializer_list<DataType>> matrix) {
   matrix_ = new DataType *[size];
   for (int i = 0; i < size; ++i) {
     matrix_[i] = new DataType[size];
   }
-
   int i = 0;
   for (auto &row : matrix) {
     int j = 0;
@@ -65,10 +64,9 @@ SquareMatrix<DataType, size>::SquareMatrix(
   }
 }
 
-template <class DataType, int size>
+template <class DataType, std::size_t size>
 SquareMatrix<DataType, size>::SquareMatrix(
     const SquareMatrix<DataType, size> &other) {
-
   matrix_ = new DataType *[size];
   for (int i = 0; i < size; ++i) {
     matrix_[i] = new DataType[size];
@@ -81,23 +79,24 @@ SquareMatrix<DataType, size>::SquareMatrix(
   }
 }
 
-template <class DataType, int size>
+template <class DataType, std::size_t size>
 DataType SquareMatrix<DataType, size>::ProductElement(
     const SquareMatrix<DataType, size> &a,
-    const SquareMatrix<DataType, size> &b, int i, int j) {
-
+    const SquareMatrix<DataType, size> &b, const int i, const int j) {
   DataType element{};
   for (int t = 0; t < size; ++t)
     element += a[i][t] * b[t][j];
-
   return element;
 }
-template <class DataType, int size> void SquareMatrix<DataType, size>::Print() {
+
+template <class DataType, std::size_t size>
+void SquareMatrix<DataType, size>::Print() const {
   for (int row = 0; row < size; ++row) {
     for (int column = 0; column < size; ++column)
       std::cout << matrix_[row][column] << " ";
     std::cout << std::endl;
   }
 }
-template <class DataType, int size>
+
+template <class DataType, std::size_t size>
 SquareMatrix<DataType, size>::~SquareMatrix() {}
