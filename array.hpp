220a48bde29a6d8a77ab8639b0ecd858fac5b249
Brocolio de la chunsa <lpradop@unsa.edu.pe>
2020-10-21 16:22:36 -0500

    bug fixes, const_iterator prototype

--- array.hpp
+++ array.hpp
@@ -15,15 +15,15 @@ public:
   array(const DataType (&array)[array_size]);
   array(const std::initializer_list<DataType> il);
   ~array();
-  constexpr std::size_t size() { return array_size; }
+  constexpr std::size_t size() const { return array_size; }
   DataType &operator[](const std::size_t index);
   const DataType &operator[](const std::size_t index) const;
   array &operator=(const array &);
   array &operator=(array &&);
   iterator begin();
-  const_iterator begin() const;
   iterator end();
-  const_iterator end() const;
+  const_iterator cbegin() const;
+  const_iterator cend() const;
 
 private:
   DataType *c_array_{new DataType[array_size]};
@@ -33,10 +33,9 @@ template <class DataType, std::size_t array_size>
 class array<DataType, array_size>::iterator {
 public:
   iterator() = default;
+  iterator(const iterator &) = default;
   iterator(const array &container, const std::size_t index,
            const bool end_flag = false);
-  iterator(const iterator &) = default;
-  iterator(iterator &&) = default;
   ~iterator() = default;
   DataType &operator*();
   bool operator==(const iterator &) const;
@@ -44,6 +43,27 @@ public:
   iterator &operator++();
   iterator &operator--();
   iterator &operator++(int);
+
+private:
+  const array &container_;
+  std::size_t index_{0};
+  bool end_flag_{false};
+};
+
+template <class DataType, std::size_t array_size>
+class array<DataType, array_size>::const_iterator {
+public:
+  const_iterator() = default;
+  const_iterator(const const_iterator &) = default;
+  const_iterator(const array &container, const std::size_t index,
+                 const bool end_flag = false);
+  ~const_iterator() = default;
+  const DataType &operator*() const;
+  bool operator==(const const_iterator &) const;
+  bool operator!=(const const_iterator &) const;
+  const_iterator &operator++();
+  const_iterator &operator--();
+  const_iterator &operator++(int);
 
 private:
   const array &container_;
@@ -84,6 +104,49 @@ bool array<DataType, array_size>::iterator::operator!=(
 template <class DataType, std::size_t array_size>
 typename array<DataType, array_size>::iterator &
 array<DataType, array_size>::iterator::operator++() {
+  if ((index_ < array_size) and (not end_flag_)) {
+    ++index_;
+    if (index_ == array_size) {
+      {
+        end_flag_ = true;
+      }
+    }
+  }
+  return *this;
+}
+
+template <class DataType, std::size_t array_size>
+array<DataType, array_size>::const_iterator::const_iterator(
+    const array &container, const std::size_t index, const bool end_flag)
+    : container_(container), index_(index), end_flag_(end_flag) {}
+
+template <class DataType, std::size_t array_size>
+const DataType &array<DataType, array_size>::const_iterator::operator*() const {
+  return container_.c_array_[index_];
+}
+
+template <class DataType, std::size_t array_size>
+bool array<DataType, array_size>::const_iterator::operator==(
+    const const_iterator &other) const {
+  if (end_flag_ and other.end_flag_) {
+    return true;
+  } else if (not(end_flag_ or other.end_flag_)) {
+    return (container_.c_array_ + index_) ==
+           (other.container_.c_array_ + index_);
+  } else {
+    return false;
+  }
+}
+
+template <class DataType, std::size_t array_size>
+bool array<DataType, array_size>::const_iterator::operator!=(
+    const const_iterator &other) const {
+  return not(*this == other);
+}
+
+template <class DataType, std::size_t array_size>
+typename array<DataType, array_size>::const_iterator &
+array<DataType, array_size>::const_iterator::operator++() {
   if ((index_ < array_size) and (not end_flag_)) {
     ++index_;
     if (index_ == array_size) {
@@ -141,9 +204,14 @@ array<DataType, array_size>::array(const DataType (&array)[array_size])
 
 template <class DataType, std::size_t array_size>
 array<DataType, array_size>::array(const std::initializer_list<DataType> il) {
-  std::size_t i = 0;
-  for (auto &e : il) {
-    c_array_[i] = e;
+  if (size() == il.size()) {
+    std::size_t i = 0;
+    for (auto &e : il) {
+      c_array_[i] = e;
+      ++i;
+    }
+  } else {
+    throw std::length_error{"sizes of arrays are not the same"};
   }
 }
 
@@ -182,6 +250,18 @@ template <class DataType, std::size_t array_size>
 typename array<DataType, array_size>::iterator
 array<DataType, array_size>::end() {
   return iterator{*this, array_size, true};
+}
+
+template <class DataType, std::size_t array_size>
+typename array<DataType, array_size>::const_iterator
+array<DataType, array_size>::cbegin() const {
+  return const_iterator{*this, 0};
+}
+
+template <class DataType, std::size_t array_size>
+typename array<DataType, array_size>::const_iterator
+array<DataType, array_size>::cend() const {
+  return const_iterator{*this, array_size, true};
 }
 
 } // namespace container
